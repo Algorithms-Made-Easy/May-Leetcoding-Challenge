bc99eaf52cdb48f4256e78f586b1c0078034e279
khushi3004 <46336275+khushi3004@users.noreply.github.com>
2020-05-18 17:32:06 +0530

    day 18

---
+++ Day18-PermutationInString.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        
+        if(s1.length() > s2.length())
+            return false;
+        
+        int[] s1Array = new int[26];
+        int[] s2Array = new int[26];
+        char[] s2c = s2.toCharArray();
+        int s1length = s1.length(); 
+        
+        for(char s1c : s1.toCharArray())
+            s1Array[s1c-'a']++;
+        
+        for(int i=0; i< s1length-1; i++)
+            s2Array[s2c[i]-'a']++;
+        
+        for(int i = s1length-1; i<s2c.length; i++){
+            s2Array[s2c[i]-'a']++;
+            if(isEqual(s2Array, s1Array))
+                return true;
+            s2Array[s2c[i-s1length+1]-'a']--;
+        }
+        return false;
+    }
+    
+    boolean isEqual(int[] s1, int[] s2) {
+        for(int i = 0; i < 26; i++){
+            if(s1[i] != s2[i])
+                return false;
+        }
+        return true;
+    }   
+}

d178acb32f7416f333b0f0391003592f620d041f
khushi3004 <46336275+khushi3004@users.noreply.github.com>
2020-05-19 21:05:48 +0530

    Day 19

---
+++ Day19-OnlineStockSpan.java
@@ -0,0 +1,50 @@
+class StockSpanner {
+    Stack<SharePrice> priceStack;
+    int day=0;
+    
+    public StockSpanner() {
+      priceStack=new Stack<>();
+    }
+    
+    public int next(int price) {
+        day++;
+       SharePrice sp=new SharePrice(day,price);
+        
+        while(!priceStack.isEmpty() && priceStack.peek().getPrice()<=price){
+            priceStack.pop();
+        }
+        if(priceStack.isEmpty()){
+            priceStack.push(sp);
+            return day;
+        }
+        int res=priceStack.peek().getDay();
+        priceStack.push(sp);
+        return day-res;
+    }
+}
+
+class SharePrice{
+    int day;
+    int price;
+    
+    SharePrice(int day,int price){
+        this.day=day;
+        this.price=price;
+    }
+    
+    public int getDay(){
+        return day;
+    }
+    
+    public int getPrice(){
+        return price;
+    }
+}
+
+
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */

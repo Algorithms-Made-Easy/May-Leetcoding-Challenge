9566f1ca7b6ac0f78f939c62caa1184da50be656
Rajat Agarwal <36561091+rajatag03@users.noreply.github.com>
2020-05-26 14:29:24 +0530

    day26

---
+++ Day26-ContiguousArray.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        
+       Map<Integer, Integer> map = new HashMap();
+        map.put(0, -1);
+        int maxLength = 0, count = 0; 
+        for(int i=0; i<nums.length; i++) {
+            
+            if(nums[i] == 0)
+                count= count-1;
+            else
+                count= count + 1;
+                
+            if(map.containsKey(count))
+                maxLength = Math.max(maxLength, i-map.get(count));
+            else
+                map.put(count, i);    
+        }
+        return maxLength;
+        
+    }
+}

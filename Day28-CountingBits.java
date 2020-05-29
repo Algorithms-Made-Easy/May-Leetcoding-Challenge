8c1a755f7c6875ac5163e358b11f27715ab4b569
Rajat Agarwal <36561091+rajatag03@users.noreply.github.com>
2020-05-30 00:41:32 +0530

    Add files via upload

--- Day28-CountingBits.java
+++ Day28-CountingBits.java
@@ -1,3 +1,20 @@
+//Pop count
+public class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        for (int i = 0; i <= num; ++i)
+            ans[i] = popcount(i);
+        return ans;
+    }
+    private int popcount(int x) {
+        int count;
+        for (count = 0; x != 0; ++count)
+          x &= x - 1; //zeroing out the least significant nonzero bit
+        return count;
+    }
+}
+
+//DP + Last Set Bit
 class Solution {
     public int[] countBits(int num) {
         int[] result=new int[num+1];
@@ -6,4 +23,33 @@ class Solution {
         }
         return result;
     }
+}
+
+//DP + Most Significant Bit
+public class Solution {
+    public int[] countBits(int num) {
+        int[] ans = new int[num + 1];
+        int i = 0, b = 1;
+        // [0, b) is calculated
+        while (b <= num) {
+            // generate [b, 2b) or [b, num) from [0, b)
+            while(i < b && i + b <= num){
+                ans[i + b] = ans[i] + 1;
+                ++i;
+            }
+            i = 0;   // reset i
+            b <<= 1; // b = 2b
+        }
+        return ans;
+    }
+}
+
+//DP + Least Significant Bit
+public class Solution {
+  public int[] countBits(int num) {
+      int[] ans = new int[num + 1];
+      for (int i = 1; i <= num; ++i)
+        ans[i] = ans[i >> 1] + (i & 1); // x / 2 is x >> 1 and x % 2 is x & 1
+      return ans;
+  }
 }

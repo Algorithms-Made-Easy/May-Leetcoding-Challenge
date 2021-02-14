d0f521bc3510e952ca8509cf755f69706e59f662
khushi3004 <46336275+khushi3004@users.noreply.github.com>
2021-02-14 14:12:22 +0530

    Update Day27-PossibleBipartitionUsingGraphColoring.java

--- Day27-PossibleBipartitionUsingGraphColoring.java
+++ Day27-PossibleBipartitionUsingGraphColoring.java
@@ -1,37 +1,67 @@
-Find Needle in haystack. How much time you need???
+class Solution {
+    
+    Map<Integer, Integer> color;
+    
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        if(dislikes.length==0) return true;
+        int[][] grid=new int[N][N];
+        
+        for(int[] i:dislikes){
+            grid[i[0]-1][i[1]-1]++;
+            grid[i[1]-1][i[0]-1]++;
+        }
+        color = new HashMap();
+        for (int node = 0; node < N; ++node){
+            if(!color.containsKey(node) && !dfs(node, 0,grid)){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    public boolean dfs(int node,int c,int[][] grid){
+        if(color.containsKey(node)){
+            return color.get(node)==c;
+        }
+        
+        color.put(node,c);
+        int[] val=grid[node];
+        for(int i=0;i<val.length;i++){
+            if(val[i]==1 && !dfs(i,c^1,grid)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
 
 
-
-Quite Similar in line with that, was today's leetcoding challenge.
-
-Finding a judge in a group of people.
-
-I am sure you guys are attempting those problems daily.
-
-And if you are stuck just REFER!!!
-
-https://lnkd.in/gatnrMK
-
-Stay connected and I hope this lockdown we all together will be able to get better out of ourselves.
-
-Here is the link to the playlist:
-https://lnkd.in/g3g7Gp7
-
-We will also be posting all the solutions at:
-https://lnkd.in/gaisfQR
-
-Like share subscribe Comment
-
-Don't forget to press the bell icon. 🔔 
-The interesting part is yet to come!!!
-
-#leetcode 
-#day10
-#codingisfun 
-#31daysChallenge 
-#may 
-#leetcoding
-#algorithmsMadeEasy
-#judge
-#coding
-#algorithm
+class Solution {
+    // union found split two group
+    // GroupA : all hate b, GroupB: all hate a; 
+    public boolean possibleBipartition(int N, int[][] dislikes) {
+        int[] group = new int[N+1];
+        for (int i = 0; i <= N; i++) {
+            group[i] = i; // initial
+        }
+        
+        for (int[] dislike : dislikes) {
+            int a = dislike[0];
+            int b = dislike[1];
+            
+            if (group[a] == a && group[b] == b) {
+                group[a] = b;
+                group[b] = a;
+            } else if (group[a] == a && group[b] != b) {
+                // let a go to group that all hate b;
+                group[a] = group[group[b]];
+            } else if (group[b] ==b && group[a] != a) {
+                group[b] = group[group[a]];
+            } else if (group[b] == group[a]) {
+                return false;
+            }
+        }
+        return true;        
+    }
+}
